5a61077f54e6c57774f8453ab16c5db1724d1aaf
Hannah Hu <112915229+Hwhuu@users.noreply.github.com>
2023-04-18 21:03:21 -0400

    Add files via upload

--- js/style.js
+++ js/style.js
@@ -1,8 +1,17 @@
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+let translated = false;
+
+document.getElementById("translate").addEventListener("click", function() { 
+  introduction = document.getElementById("intro");
+  aboutme = document.getElementById("about");
+  if(translated) { 
+    introduction.innerHTML = "Woof woof woof woof<br>"
+    aboutme.innerHTML = "Woof woof woof woof woof WOOF woof WOof Woof Woof woof woof WOOOOF! Woof woof woof woof woof WOOF woof WOof Woof Woof woof woof Woof. Woof woof woof woof woof WOOF woof WOof Woof Woof woof woof Woooof. Woof woof woof woof woof WOOF woof WOof Woof Woof woof woof WOOOOF!!"
+    translated = false;
   }
+  else { 
+    introduction.innerHTML = "My name is Yuki!"
+    aboutme.innerHTML = "I was born on October 1st, 2018. I am currently 4 years old. I am a maltipoo (aka maltese and poodle mix). When it's nice out, I love to go on walks in the neighborhood with mom or sister. When I'm not out and about, you can usually find me napping in my favorite spot on the couch or cuddling with my favorite human. I have also mastered the art of a good puppy eye, my humans can't resist my charm and always gives me treats. Please take a look at my favorite doggy recipes or more fun pictures of me. Woof!"
+    translated = true;
+  }
+})
+
